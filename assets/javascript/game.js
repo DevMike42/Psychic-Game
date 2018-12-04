96b3499f6c499301b9f83b6854213a3443e0638b
Mike Rivera <michaelrivera@michaels-mbp-2.attlocal.net>
2018-12-03 23:14:14 -0800

    Javascript almost done / needs debugging

---
+++ assets/javascript/game.js
@@ -0,0 +1,97 @@
+// VARIABLES DEFINED FOR GAME
+// ===================================================================
+var winCount = 1;
+var lossCount = 0;
+var guessesLeft = 10;
+var guessedLetters = [];
+
+// Array of alphabet
+var alphabet = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
+    , "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", 
+    "x", "y", "z"];
+
+// Computer picks random "letter" or value from alphabet array
+var randomLetter = Math.floor(Math.random() * alphabet.length);
+var computerChoice = alphabet [randomLetter];
+
+console.log ("Computer choice = " + computerChoice);
+
+// GENERATES RANDOM CUMPUTER CHOICE AND ALLOWS USER TO CHOOSE LETTERS
+// ===================================================================
+
+// Listener for user key press
+document.onkeyup = function (event) {
+    // Stores user key press into userChoice variable and
+    // converts to lowercase to avoid uppercase choices
+    // from being missed
+    var userChoice = event.key.toLowerCase();
+
+    // RegExp object for confirming that userChoice is a
+    // letter from the alphabet array
+    var regexp = /[a-z]/gi;
+
+        // Alerts user to try again if key other than letter is chosen
+        if (!regexp.test (userChoice)) {
+            alert ("Letters are in the alphabet dummy! Try again");
+        }
+
+        else {
+        }
+
+// If statmens that determine wins and loses. Also tracks progress
+// ===================================================================
+
+        if (guessesLeft <= 0) {
+            lossCount++;
+            document.getElementById ("lossCounty").innerHTML = lossCount++;
+            console.log ("You lost");
+            alert ("You lost!");
+            guessesLeft = 10;
+            guessedLetters = [];
+            document.getElementById ("guessedLetters").innherHTML = guessedLetters;
+            document.getElementById ("guessesLeft").innerHTML = 10;
+            randomIndex = Math.floor(Math.random() * alphabet.length);
+            computerChoice = alphabet [randomLeter];
+            console.log ("Computer choice = " + computerChoice);
+        }
+
+        if (computerChoice == userChoice) {
+            console.log ("You won!");
+            alert ("You won!");
+            document.getElementById ("winCount").innerHTML = winCount++;
+            guessedLetters = [];
+            document.getElementById ("guessedLetters").innerHTML = guessedLetters;
+            computerChoice = alphabet [randomLetter];
+            console.log ("Computer choice = " + computerChoice);
+            guessesLeft = 10;
+            document.getElementById ("guessesLeft").innerHTML = 10;
+        }
+
+        else {
+            console.log ("Guess again!");
+            document.getElementById ("guessesLeft").innerHTML = guessesLeft;
+            guessedLetters.push (userChoice);
+            document.getElementById ("guessedLetters").innerHTML = guessedLetters;
+        }
+
+        // Not working: Computer's generates random letter but keeps choosing same letter
+        // Not working: Guesses left counter does not lower and reset guesses
+}
+
+
+
+
+// Look for key press and log which key was pressed (write to page "Your guesses so far")
+
+
+// Determine and display amount of guesses left
+
+
+// Determine and display amount of guesses left
+
+
+// Track if user wins or loses and reset game
+
+
+// Track wins and loses
